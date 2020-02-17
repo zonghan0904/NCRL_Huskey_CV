4e53092c13deb1d6912311c60a848fb29fa269a8
zonghan <steven.me05@nctu.edu.tw>
2020-02-17 18:04:23 +0800

    add configuration option in take_pic.

--- take_pic.py
+++ take_pic.py
@@ -5,11 +5,13 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while 1:
     cv2.imshow("img1", frame)
     if (cv2.waitKey(1) & 0xFF == ord('y')):
-        cv2.imwrite("images/pic2.png", frame)
+        cv2.imwrite("images/pic3.png", frame)
         cv2.destroyAllWindows()
         break
     else:
